4526edd821b5cd3cb561949d48bd1bff58e74fc1
Mathieu Debove <mad@teecu.be>
2016-11-19 21:24:23 +0100

    Let's get it start3d!

---
+++ src/main/java/t3/toe/installer/envinfo/TIBCOEnvironmentMarshaller.java
@@ -0,0 +1,36 @@
+/**
+ * (C) Copyright 2016-2016 teecube
+ * (http://teecu.be) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package t3.toe.installer.envinfo;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import com.tibco.envinfo.ObjectFactory;
+import com.tibco.envinfo.TIBCOEnvironment;
+
+import t3.xml.XMLMarshall;
+
+public class TIBCOEnvironmentMarshaller extends XMLMarshall<TIBCOEnvironment, ObjectFactory> {
+
+	public static final String NAMESPACE = "http://tibco.com/envInfo";
+
+	public TIBCOEnvironmentMarshaller(File xmlFile) throws JAXBException {
+		super(xmlFile);
+	}
+
+}
